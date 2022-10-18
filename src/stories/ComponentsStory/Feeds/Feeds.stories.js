26182ee5df3f2e008f1e7f6b3a8877cbed6a7e1f
Dharmik Patel <dharmikp.mobio@gmail.com>
2022-10-18 11:56:07 +0530

    new added

---
+++ src/stories/ComponentsStory/Feeds/Feeds.stories.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Feeds from './Feeds'
+// import './sidebar.css'
+
+export default {
+  title: 'Tailwindui/Feeds',
+  component: Feeds
+}
+
+// export const Small = () => <Sidebar size='small' placeholder='Small size' />
+export const Medium = () => <Feeds size='medium' placeholder='Medium size' />
+// export const Large = () => <Sidebar size='large' placeholder='Large size' />
---
+++ src/stories/ComponentsStory/Tables/Tables.stories.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Tables from './Tables'
+// import './sidebar.css'
+
+export default {
+  title: 'Tailwindui/Tables',
+  component: Tables
+}
+
+// export const Small = () => <Sidebar size='small' placeholder='Small size' />
+export const Medium = () => <Tables size='medium' placeholder='Medium size' />
+// export const Large = () => <Sidebar size='large' placeholder='Large size' />
